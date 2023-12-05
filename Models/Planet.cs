93259cb435316c2f6f2d8f62a08eb03ea827807a
Alexandr Cocier <licoms1997@gmail.com>
2023-12-05 10:34:14 +0200

    Create Planet.cs model

---
+++ Models/Planet.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiPlanets.Models
+{
+    public class Planet
+    {
+        public string Name { get; set; }
+        public string Subtitle { get; set; }
+        public string HeroImage { get; set; }
+        public string Description { get; set; }
+        public Color AccentColorStart { get; set; }
+        public Color AccentColorEnd { get; set; }
+        public List<string> Images { get; set; }
+    }
+}
